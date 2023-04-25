6e2656bdc2f7545af3f9ad29964db971c9c62fca
muhammad fajar <invasionfajar@gmail.com>
2023-04-26 06:38:33 +0700

    update file resume

--- src/pages/api/download.ts
+++ src/pages/api/download.ts
@@ -2,12 +2,12 @@ import path from "path";
 import fs from 'fs';
 
 export default function handler(req: any, res: any){
-    const filePath = path.join(process.cwd(), 'public', 'pdfs', `resume.pdf`);
+    const filePath = path.join(process.cwd(), 'public', 'pdfs', `mfajarna_resume.pdf`);
     const stat = fs.statSync(filePath);
     const fileStream = fs.createReadStream(filePath);
 
     res.setHeader('Content-Length', stat.size);
     res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', `attachment; filename=resume.pdf`);
+    res.setHeader('Content-Disposition', `attachment; filename=mfajarna_resume.pdf`);
     fileStream.pipe(res);
 }
