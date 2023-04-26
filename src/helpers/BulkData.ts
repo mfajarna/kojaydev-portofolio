08a4530383650f80569efdc555c0cdb430881a87
muhammad fajar <invasionfajar@gmail.com>
2023-04-26 18:29:00 +0700

    Update projects thumbnail

--- src/helpers/BulkData.ts
+++ src/helpers/BulkData.ts
@@ -3,42 +3,42 @@ const dataProjects = [
         id: 1,
         title: 'Mobile TopUp',
         tech: 'React Native',
-        projectStorage: '/assets/projects1.png',
+        projectStorage: '/assets/projects/mobiletopup.png',
         projectUrl: 'https://gitlab.com/mfajarna/bacakode-topup-rn'
     },
     {
         id: 2,
         title: 'E-Puskesmas',
         tech: 'Laravel Framework',
-        projectStorage: '/assets/projects2.png',
+        projectStorage: '/assets/projects/puskeslinggar.png',
         projectUrl: 'https://github.com/mfajarna/epuskesmas-backend'
     },
     {
         id: 3,
         title: 'Mobile Akademik',
         tech: 'React Native',
-        projectStorage: '/assets/projects/mobileakademik2.png',
+        projectStorage: '/assets/projects/mobileakademik3.png',
         projectUrl: ' '
     },
     {
         id: 4,
         title: 'E-Puskesmas App',
         tech: 'React Native',
-        projectStorage: '/assets/projects/e-puskesmas.png',
+        projectStorage: '/assets/projects/epuskesmas.png',
         projectUrl: 'https://github.com/mfajarna/epuskesmas'
     },
     {
         id: 5,
         title: 'Hasnida Collection App',
         tech: 'React Native',
-        projectStorage: '/assets/projects/hasnida.png',
+        projectStorage: '/assets/projects/hasnidapic.png',
         projectUrl: 'https://github.com/mfajarna/hasnidacollection'
     },
     {
         id: 6,
         title: 'Information SlideShow',
         tech: 'NextJS',
-        projectStorage: '/assets/projects/slideshow.png',
+        projectStorage: '/assets/projects/slideshowpic.png',
         projectUrl: ' '
     },
 ]
